1af6c2be9ae952ed5099af2428fa69b57341976d
Johannes Wagener <johannes@wagener.cc>
2012-02-02 00:26:02 -0800

    added first basic test suite

---
+++ test/test.js
@@ -0,0 +1,24 @@
+function setupRecorder(swfSrc, callback){
+  Recorder.initialize({
+    "swfSrc": swfSrc,
+    initialized: callback
+  });  
+}
+
+function setupTests(){
+  test("Recorder.swf Exposed Function", function() {
+    var fi = Recorder.flashInterface();
+    ok(fi.record            != undefined, "record()");
+    ok(fi._stop             != undefined, "_stop()");
+    ok(fi._play             != undefined, "_play()");
+    ok(fi.upload            != undefined, "upload()");
+    ok(fi.audioData         != undefined, "audioData()");
+    ok(fi.showFlash         != undefined, "showFlash()");
+    ok(fi.debugLog          != undefined, "debugLog()");
+    ok(fi.recordingDuration != undefined, "recordingDuration()");
+  });
+}
+
+$(function(){
+  setupRecorder("../flash/bin-release/Recorder.swf", setupTests);
+});
